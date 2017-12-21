c50d0a6efbef0fb54e02e3de7128dc526cb3ddea
adam <adam.kovesdi.net@gmail.com>
2017-12-21 09:22:16 +0100

    RSS test

---
+++ feedtest.rb
@@ -0,0 +1,11 @@
+require 'rss'
+
+feed_urls = [ "http://feeds.bbci.co.uk/news/business/rss.xml", "http://feeds.bbci.co.uk/news/politics/rss.xml", "http://feeds.bbci.co.uk/sport/football/rss.xml?edition=int" ]
+
+
+feed = RSS::Parser.parse(feed_urls[0])
+feed.items.each do |item|
+	puts "#{item.pubDate}|#{item.title}|#{item.description}"
+
+end
+
